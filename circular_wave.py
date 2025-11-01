b6c6f40a91c7b4c454adf6a72d5a2f76d954115a
Manthan Sahu <manthansahusahu@gmail.com>
2025-11-01 18:07:48 +0530

    Update circular_wave.py

--- circular_wave.py
+++ circular_wave.py
@@ -1,19 +1,20 @@
 import turtle
 import math
 
-turtle.bgcolor("black")
+screen = turtle.Screen()
+screen.bgcolor("black")
+
 t = turtle.Turtle()
 t.speed(0)
-turtle.colormode(255)
 
-colors = [(255,0,0),(255,127,0),(255,255,0),(0,255,0),(0,0,255),(75,0,130)]
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 for i in range(360):
     t.pencolor(colors[i % len(colors)])
     t.penup()
-    t.goto(0,0)
+    t.goto(0, 0)
     t.pendown()
-    t.forward(100 + 50 * math.sin(math.radians(i*3)))
+    t.forward(100 + 50 * math.sin(math.radians(i * 3)))
     t.right(1)
 
 turtle.done()
