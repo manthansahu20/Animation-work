6bd9e40e7dfa5ab1796b7e030c938e46a0ccf427
Manthan Sahu <manthansahusahu@gmail.com>
2025-11-02 09:54:29 +0530

    Update spiral_ring.py

--- spiral_ring.py
+++ spiral_ring.py
@@ -1,14 +1,18 @@
 import turtle
 
-turtle.bgcolor("black")
+# Create a screen object and set background color
+screen = turtle.Screen()
+screen.bgcolor("black")
+
 t = turtle.Turtle()
 t.speed(0)
-turtle.colormode(255)
-colors = [(255,0,0),(255,127,0),(255,255,0),(0,255,0),(0,0,255),(75,0,130)]
+
+# Use color names (Trinket doesn't support RGB tuples)
+colors = ["red", "orange", "yellow", "green", "blue", "indigo"]
 
 for i in range(36):
     t.pencolor(colors[i % len(colors)])
-    t.circle(50 + i*5)
+    t.circle(50 + i * 5)
     t.right(10)
 
 turtle.done()
